0cb2ac8c81f54c805005b6aba7bf216e9b83616d
phuc-ueters <tranthanhphuc12a5@gmail.com>
2022-12-11 16:44:20 +0700

    add pycaret

---
+++ utils/mape.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def mean_absolute_percentage_error(y_true, y_pred,
+                                   sample_weight=None,
+                                   multioutput='uniform_average'):
+    epsilon = np.finfo(np.float64).eps
+    mape = np.abs(y_pred - y_true) / np.maximum(np.abs(y_true), epsilon)
+    output_errors = np.average(mape,
+                               weights=sample_weight, axis=0)
+    if isinstance(multioutput, str):
+        if multioutput == 'raw_values':
+            return output_errors
+        elif multioutput == 'uniform_average':
+            # pass None as weights to np.average: uniform mean
+            multioutput = None
+
+    return np.average(output_errors, weights=multioutput)
